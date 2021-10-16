a52d9079c6ddd4f5edbe5c2f13462de4bdf24872
Milos Petrusic <petrusicmilos94@gmail.com>
2021-10-16 19:56:55 +0200

    Custom TableViewCell created along with it's viewModel for AddEventVC.

--- EventsApp/Controllers/AddEventViewController.swift
+++ EventsApp/Controllers/AddEventViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class AddEventViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     var viewModel: AddEventViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupViews()
+        viewModel.onUpdate = { [weak self] in
+            self?.tableView.reloadData()
+        }
+        viewModel.viewDidLoad()
         
     }
     
@@ -23,8 +29,55 @@ class AddEventViewController: UIViewController {
         viewModel.viewDidDisappear()
     }
     
+    private func setupViews() {
+        tableView.dataSource = self
+        tableView.register(TitleSubtitleCell.self, forCellReuseIdentifier: "TitleSubtitleCell")
+        navigationItem.title = viewModel.title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(tappedDone))
+        navigationController?.navigationBar.tintColor = .black
+        
+        //to force large title
+        tableView.contentInsetAdjustmentBehavior = .never
+        tableView.setContentOffset(.init(x: 0, y: -1), animated: false)
+    }
+    
+    @objc func tappedDone() {
+        viewModel.tappedDone()
+    }
+    
     deinit {
         print("deinit from the add event vc")
     }
+}
 
+extension AddEventViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = viewModel.cell(for: indexPath)
+        switch cell {
+        case .titleSubtitle(let titleSubtitleCellViewModel):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TitleSubtitleCell", for: indexPath) as! TitleSubtitleCell
+            cell.update(with: titleSubtitleCellViewModel)
+            cell.subtitleTextField.delegate = self
+            return cell
+        }
+    }
+}
+
+extension AddEventViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let currentText = textField.text else { return false }
+        let text = currentText + string
+        
+        let point = textField.convert(textField.bounds.origin, to: tableView)
+        if let indexPath = tableView.indexPathForRow(at: point) {
+            viewModel.updateCell(indexPath: indexPath, subtitle: text)
+        }
+        
+        return true
+    }
 }
--- EventsApp/ViewModels/AddEventViewModel.swift
+++ EventsApp/ViewModels/AddEventViewModel.swift
@@ -6,12 +6,56 @@
 //
 
 import Foundation
+import UIKit
 
 final class AddEventViewModel {
+    
+    let title = "Add"
+    var onUpdate: () -> Void = {}
+    
+    enum Cell {
+        case titleSubtitle(TitleSubtitleCellViewModel)
+    }
+    
+    private(set) var cells: [AddEventViewModel.Cell] = []
     var coordinator: AddEventCoordinator?
+    
+    func viewDidLoad() {
+        cells = [
+            .titleSubtitle(TitleSubtitleCellViewModel(title: "Name", subtitle: "", placeholder: "Add a name", type: .text, onCellUpdate: {})),
+            .titleSubtitle(TitleSubtitleCellViewModel(title: "Date", subtitle: "", placeholder: "Select a date", type: .date, onCellUpdate: { [weak self] in
+                self?.onUpdate()
+            })),
+            .titleSubtitle(TitleSubtitleCellViewModel(title: "Background", subtitle: "", placeholder: "", type: .image, onCellUpdate: { [weak self] in
+                self?.onUpdate()
+            }))
+        ]
+        onUpdate()
+    }
     
     func viewDidDisappear() {
         coordinator?.didFinishAddEvent()
+    }
+    
+    func tappedDone() {
+        print("Tapped Done, I will save model now.")
+        // save model to core data
+        // tell coordinator to dismiss vc
+    }
+    
+    func updateCell(indexPath: IndexPath, subtitle: String) {
+        switch cells[indexPath.row] {
+        case .titleSubtitle(let titleSubtitleCellViewModel):
+            titleSubtitleCellViewModel.update(subtitle)
+        }
+    }
+    
+    func numberOfRows() -> Int {
+        return cells.count
+    }
+    
+    func cell(for indexPath: IndexPath) -> Cell {
+        return cells[indexPath.row]
     }
     
     deinit {
